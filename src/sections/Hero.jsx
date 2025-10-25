557bb0a2808bb633c89dde72869b56214ebb85b4
pramodya112 <pramodyawarnakula295.com>
2025-10-26 04:23:07 +0530

    create content of the hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,50 @@
+import Button from "../components/Button"
+import { words } from "../constants"
+
+const Hero = () => {
+    return (
+        <section id="hero" className="relative overflow-hidden">
+            <div className="absolute top-0 left-0 z-10">
+                <img src="/images/bg.png" alt="background" />
+            </div>
+
+            <div className="hero-layout">
+                {/* left side - hero content */}
+                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+                    <div className="flex flex-col gap-7">
+                        <div className="hero-text">
+                            <h1>Shaping
+                                <span className="slide">
+                                    <span className="wrapper">
+                                        {words.map((word) => (
+                                            <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                                <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                                                <span>{word.text}</span>
+                                            </span>
+                                        ))}
+                                    </span>
+                                </span>
+                            </h1>
+                            <h1>into Real Projects</h1>
+                            <h1>that Deliver Results</h1>
+                        </div>
+                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">Hi, I'm Vichith, a mobile app developer specialized in
+                            Flutter and Kotlin, passionate about
+                            creating high-quality, efficient
+                            applications and contributing to
+                            innovative projects while driving
+                            continuous improvement and
+                            professional growth
+                        </p>
+                        <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="see my work" />
+                    </div>
+
+                </header>
+                {/* right side - 3d model */}
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero
